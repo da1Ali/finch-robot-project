23bcdad66a84592679e394309398b4969e60dc1b
da1Ali <aakhtar0003@mymail.lausd.net>
2025-06-03 13:22:53 -0700

    inint commit

---
+++ src/main/java/com/ali/FinalFinch.java
@@ -0,0 +1,15 @@
+package com.ali;
+
+import com.birdbrain.Finch;
+
+public class FinalFinch {
+    public static void main(String[] args) {
+        Finch myFinch = new Finch();
+
+        // WHat do I want my finch to do?
+
+
+        myFinch.stopAll();
+        myFinch.disconnect();
+    }
+}
